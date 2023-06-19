60a6e28d568081e0e5664567a61eca83cc9c55c7
faz <ahmedfayez834@gmail.com>
2023-06-19 15:16:02 +0300

    user update

--- lib/authentication/data/mapper/patient_mapper.dart
+++ lib/authentication/data/mapper/patient_mapper.dart
@@ -1,37 +1,14 @@
+import 'package:health_care/authentication/data/mapper/user_mapper.dart';
 import 'package:health_care/authentication/data/response/patient_response/patient_response.dart';
 import 'package:health_care/authentication/domain/model/patient_model.dart';
 import 'package:health_care/core/utils/constants.dart';
 import 'package:health_care/core/utils/extension.dart';
 
-extension PatientUserResponseMapper on PatientUserResponse? {
-  PatientUser toDomain() {
-    return PatientUser(
-      this?.appointments?.orEmptyList() ?? Constants.emptyList,
-      this?.id?.orEmpty() ?? Constants.empty,
-      this?.name?.orEmpty() ?? Constants.empty,
-      this?.email?.orEmpty() ?? Constants.empty,
-      this?.confirmed?.orFalse() ?? Constants.FALSE,
-      this?.active?.orFalse() ?? Constants.FALSE,
-      this?.type?.orEmpty() ?? Constants.empty,
-      this?.userName?.orEmpty() ?? Constants.empty,
-      this?.v?.orZero() ?? Constants.zero,
-    );
-  }
-}
-
-extension PatientDataResponseMapper on PatientDataResponse? {
-  PatientData toDomain() {
-    return PatientData(
-      this?.user?.toDomain(),
-    );
-  }
-}
-
 extension PatientAuthResponseMapper on PatientAuthResponse? {
   PatientAuth toDomain() {
     return PatientAuth(
       this?.token?.orEmpty() ?? Constants.empty,
-      this?.data?.toDomain(),
+      this?.user?.toDomain(),
     );
   }
 }
