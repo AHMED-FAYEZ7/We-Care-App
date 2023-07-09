a709c0beee96c88326a18f2e63762a9880a73b35
faz <ahmedfayez834@gmail.com>
2023-07-09 04:18:31 +0300

    refactor design

--- lib/patient/presentation/screens/appointment/widget/appointment_widget.dart
+++ lib/patient/presentation/screens/appointment/widget/appointment_widget.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:health_care/core/global/resources/icons_manger.dart';
 import 'package:health_care/core/global/resources/values_manger.dart';
 import 'package:health_care/core/global/theme/app_color/color_manager.dart';
 import 'package:health_care/patient/domain/model/appointment_model.dart';
